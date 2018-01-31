d99592abdcc728c6fa0d64973d9fc119ab074a70
zwug <zwugme@gmail.com>
2018-01-31 23:14:24 +0300

    added self-made slider concept

--- src/components/landing/SliderSection/SliderSection.jsx
+++ src/components/landing/SliderSection/SliderSection.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
 
-import promo1Img from '../../../assets/img/promo-1.jpg'
+// import promo1Img from '../../../assets/img/promo-1.jpg'
 
 import './SliderSection.css';
 
 class SliderSection extends Component {
   render() {
     return (
-      <section className="landing-slider-section">
+      <section className="landing-slider">
         <h1>Танцы в тридевятом</h1>
-        <Carousel>
-            <div>
-                <img src={promo1Img} />
-                <p className="legend">Legend 1</p>
-            </div>
-        </Carousel>
+        <div className="landing-slider-container">
+          <div className="promo-img promo-img-1"></div>
+          <div className="promo-img promo-img-2"></div>
+        </div>
+        <div className="landing-slider-bottom">
+          <p>Поговаривают, что после наших мероприятий еще никто не уходил с плохим настроением</p>
+          <button className="landing-slider-bottom-btn">Участвовать</button>
+        </div>
       </section>
     );
   }
